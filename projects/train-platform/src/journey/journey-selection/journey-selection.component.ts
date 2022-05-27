e7159ced97642c802c40f0467ff47210fa821dd3
Konstantin Tieber <hello@xkons.de>
2022-05-27 17:35:00 +0200

    Update to angular 13

--- projects/train-platform/src/journey/journey-selection/journey-selection.component.ts
+++ projects/train-platform/src/journey/journey-selection/journey-selection.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Journey } from '../journey.type';
 import { JourneysService } from '../journeys.service';
@@ -8,12 +8,10 @@ import { JourneysService } from '../journeys.service';
   templateUrl: './journey-selection.component.html',
   styleUrls: ['./journey-selection.component.css'],
 })
-export class JourneySelectionComponent implements OnInit {
+export class JourneySelectionComponent {
   journeys$: Observable<Journey[]>;
 
   constructor(journeysService: JourneysService) {
     this.journeys$ = journeysService.loadJourneys();
   }
-
-  ngOnInit(): void {}
 }
