ae0872bbd38d38a468fa530b854ea50cce61e9df
Konstantin Tieber <hello@xkons.de>
2022-05-28 19:39:31 +0200

    Step 8: Deploy shell app and microfrontend to netlify

--- projects/bookings/src/environments/environment.prod.ts
+++ projects/bookings/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   embedded: true,
-  assetUrl: '', // TODO: set correct assetUrl
+  assetUrl: '/frontends/bookings',
 };
--- projects/train-platform/src/app/micro-frontends/bookings-host.module.ts
+++ projects/train-platform/src/app/micro-frontends/bookings-host.module.ts
@@ -1,16 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { environment } from '../../environments/environment';
 import { BookingsHostComponent } from './bookings-host.component';
 import { LoadMicroFrontendGuard } from './load-micro-frontend.guard';
 import { MicroFrontendLanguageDirective } from './micro-frontend-language.directive';
 import { MicroFrontendRoutingDirective } from './micro-frontend-routing.directive';
+
+const getMicrofrontendBundleUrl = (frontendName: 'bookings') =>
+  `/frontends/${frontendName}/main.js`;
 
 const routes: Routes = [
   {
     path: '**',
     canActivate: [LoadMicroFrontendGuard],
     data: {
-      bundleUrl: 'http://localhost:4201/main.js',
+      bundleUrl: environment.production
+        ? getMicrofrontendBundleUrl('bookings')
+        : 'http://localhost:4201/main.js',
     },
     component: BookingsHostComponent,
   },
