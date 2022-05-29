35e841f7cee8775268d9821878ec8aa182d24da8
Konstantin Tieber <hello@xkons.de>
2022-05-29 19:19:54 +0200

    Deploy to Netlify

--- projects/train-platform/src/micro-frontends/bookings-host.module.ts
+++ projects/train-platform/src/micro-frontends/bookings-host.module.ts
@@ -1,9 +1,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
 import { BookingsHostComponent } from './bookings-host.component';
 import { LoadMicroFrontendGuard } from './load-micro-frontend.guard';
 import { MicroFrontendLanguageDirective } from './micro-frontend-language.directive';
 import { MicroFrontendRoutingDirective } from './micro-frontend-routing.directive';
+
+const getMicrofrontendBundleUrl = (frontendName: 'bookings') =>
+  `/frontends/${frontendName}/main.js`;
 
 @NgModule({
   declarations: [
@@ -18,7 +22,9 @@ import { MicroFrontendRoutingDirective } from './micro-frontend-routing.directiv
         canActivate: [LoadMicroFrontendGuard],
         component: BookingsHostComponent,
         data: {
-          bundleUrl: 'http://localhost:4201/main.js',
+          bundleUrl: environment.production
+            ? getMicrofrontendBundleUrl('bookings')
+            : 'http://localhost:4201/main.js',
         },
       },
     ]),
